d6f67a719c175277fc64c6e7d9b3562a3bc40d87
tykervella <kervellatyler@gmail.com>
2023-07-03 05:03:14 -0700

    fixing routes?

--- src/App.js
+++ src/App.js
@@ -43,40 +43,37 @@ function App() {
         <Navbar />
         <div className="container" style={{ marginBottom: "2.5%", width: "82.5%" }}>
           <Routes>
-            <Route
-              path="/slayhaven" // Set "/slayhaven" as the base path
-              element={
+            
                 <>
-                  <Route path="/" element={<Home />} />
+                  <Route path="/slayhaven" element={<Home />} />
 
                   {/* about routes */}
-                  <Route path="/information" element={<Information />} />
-                  <Route path="/campuslife" element={<CampusLife />} />
-                  <Route path="/faculty" element={<Faculty />} />
+                  <Route path="/slayhaven/information" element={<Information />} />
+                  <Route path="/slayhaven/campuslife" element={<CampusLife />} />
+                  <Route path="/slayhaven/faculty" element={<Faculty />} />
 
                   {/* location routes */}
-                  <Route path="/central" element={<Central />} />
-                  <Route path="/lorehold" element={<Lorehold />} />
-                  <Route path="/prismari" element={<Prismari />} />
-                  <Route path="/quandrix" element={<Quandrix />} />
-                  <Route path="/silverquill" element={<Silverquill />} />
-                  <Route path="/witherbloom" element={<Witherbloom />} />
+                  <Route path="/slayhaven/central" element={<Central />} />
+                  <Route path="/slayhaven/lorehold" element={<Lorehold />} />
+                  <Route path="/slayhaven/prismari" element={<Prismari />} />
+                  <Route path="/slayhaven/quandrix" element={<Quandrix />} />
+                  <Route path="/slayhaven/silverquill" element={<Silverquill />} />
+                  <Route path="/slayhaven/witherbloom" element={<Witherbloom />} />
 
                   {/* Faculty Routes  */}
-                  <Route path="/npcs/faculty/central" element={<CentralFaculty />} />
-                  <Route path="/npcs/faculty/lorehold" element={<LoreholdFaculty />} />
-                  <Route path="/npcs/faculty/prismari" element={<PrismariFaculty />} />
-                  <Route path="/npcs/faculty/quandrix" element={<QuandrixFaculty />} />
-                  <Route path="/npcs/faculty/silverquill" element={<SilverquillFaculty />} />
-                  <Route path="/npcs/faculty/witherbloom" element={<WitherbloomFaculty />} />
+                  <Route path="/slayhaven/npcs/faculty/central" element={<CentralFaculty />} />
+                  <Route path="/slayhaven/npcs/faculty/lorehold" element={<LoreholdFaculty />} />
+                  <Route path="/slayhaven/npcs/faculty/prismari" element={<PrismariFaculty />} />
+                  <Route path="/slayhaven/npcs/faculty/quandrix" element={<QuandrixFaculty />} />
+                  <Route path="/slayhaven/npcs/faculty/silverquill" element={<SilverquillFaculty />} />
+                  <Route path="/slayhaven/npcs/faculty/witherbloom" element={<WitherbloomFaculty />} />
 
                   {/* Student Routes  */}
                   {/* Resources Routes  */}
-                  <Route path="/archetypes" element={<Archetypes />} />
-                  <Route path="/creation" element={<Creation />} />
+                  <Route path="/slayhaven/archetypes" element={<Archetypes />} />
+                  <Route path="/slayhaven/creation" element={<Creation />} />
                 </>
-              }
-            />
+             
           </Routes>
         </div>
         {/* <Footer /> */}
