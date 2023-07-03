b15e1ebb34da157407ed9584faee49d21624090d
tykervella <kervellatyler@gmail.com>
2023-07-03 04:32:26 -0700

    fixed text on general information page

--- src/pages/about/Information.js
+++ src/pages/about/Information.js
@@ -166,30 +166,27 @@ export default function Information() {
           </Col>
         </Row>
 
-        <Row style={{display:"flex", justifyContent:"space-between"}}>
-          <Col md={6}>
+        <Row >
+          <Col md={6} style={{display:"flex", flexDirection:"column", justifyContent: "center", alignItems: "center"}}>
             <img 
                 src={Archaics} 
                 style={{ width: "100%", marginBottom: "2.5%"}} 
                 alt="Image of an Archaic" 
               />
           </Col>
-          <Col md={6} style={{display:"flex",alignItems: "center"}}>
-            <p>
-              The <b><u>Oracle of Strixhaven</u></b>, selected by the Founder Dragons, is often said to be the wisest and most accomplished mage in the multiverse. The Oracle’s lifelong task is to ensure that magic is used to help people and not twisted to evil ends. To be the Oracle, one must understand fundamental truths about the nature of magic, know and wield hundreds of spells, and possess impeccable judgment and virtue. The current Oracle is known as Jadzi. 
-            </p>
+          <Col md={6} style={{display:"flex", flexDirection:"column", justifyContent: "center", alignItems: "center"}}>
+          <p>
+            Mysteriously linked to the Oracle, <b><u>archaics</u></b> are wise, giant, long-lived beings with an innate talent for magic. They can be seen striding through the wilds, exploring sources of magic with their many arms or contemplating existence through their “eye,” which functions as a magical focus of some kind. Scholars seek out archaics for their vast knowledge of history and magic, but archaics tend to communicate in obscure allusions and cryptic metaphors.
+          </p>
+          <p> 
+            Few know that archaics’ existence is linked to a time-warping phenomenon involving the Oracle. When an Oracle dies, their mind and spirit is swept off to the distant past, drawn backward through time toward the intense magical power that brought the world into being. Splinters of that Oracle’s soul and its fragmented memories coalesce into a newborn archaic. Every archaic alive today was born at the dawn of time from the mind of someone who has lived and died (or who will one day live and die) as an Oracle. Archaics speak in cryptic allegories not only to tease and test the eager minds of young mages, but also to cleverly sidestep time paradoxes.
+          </p>
           
           </Col>
           
         </Row>
 
-        <p>
-            Mysteriously linked to the Oracle, <b><u>archaics</u></b> are wise, giant, long-lived beings with an innate talent for magic. They can be seen striding through the wilds, exploring sources of magic with their many arms or contemplating existence through their “eye,” which functions as a magical focus of some kind. Scholars seek out archaics for their vast knowledge of history and magic, but archaics tend to communicate in obscure allusions and cryptic metaphors.
-        </p>
-
-        <p> 
-          Few know that archaics’ existence is linked to a time-warping phenomenon involving the Oracle. When an Oracle dies, their mind and spirit is swept off to the distant past, drawn backward through time toward the intense magical power that brought the world into being. Splinters of that Oracle’s soul and its fragmented memories coalesce into a newborn archaic. Every archaic alive today was born at the dawn of time from the mind of someone who has lived and died (or who will one day live and die) as an Oracle. Archaics speak in cryptic allegories not only to tease and test the eager minds of young mages, but also to cleverly sidestep time paradoxes.
-        </p>
+        
         
       </Row>
     </Container>
